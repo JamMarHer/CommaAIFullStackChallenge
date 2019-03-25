798588f605d5b51ec94d520b2cb0a22e8813094d
Jam Hernandez <jamarck96@gmail.com>
2019-03-25 19:01:19 -0400

    started working on flask

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,10 +1,14 @@
 import json
+import math
+from datetime import datetime
 from os import listdir
 from os.path import isfile, join
+import pickle
 
 class DataProcessing(object):
 
-    GEOJSON_LINE_SCHEMA_PATH = "geoJsonSchemas/pathSchema.json"
+    GEOJSON_LINE_SCHEMA_PATH = "JsonSchemas/geoPathSchema.json"
+    VEGA_GRAPH_SCHEMA_PATH = "JsonSchemas/graph.json"
 
     """docstring for DataProcessing."""
 
@@ -12,32 +16,55 @@ class DataProcessing(object):
         super(DataProcessing, self).__init__()
         self.path = path
 
-    def getAllTripsColorLineParams(self):
+    def getAllTripsData(self):
         allData = dict()
         listOfTrips = [f for f in listdir(self.path) if isfile(join(self.path, f))]
+        cachedData = self.loadData()
+        if (cachedData):
+            return cachedData
         for tripFile in listOfTrips:
-            g, s, d = self.getColorLineParams(self.loadTripByName(tripFile))
+            tripRawData = self.loadTripByName(tripFile)
+            g, s, d, se= self.getColorLineParams(tripRawData)
+            graphData = self.getTripGraph(location=g, speed=s, distance=d,
+                                                                startEnd=se)
+
             allData[tripFile] = {
                 "tripName": tripFile.split(".")[0],
                 "location": g,
                 "speed": s,
-                "distance": d
+                "distance": d,
+                "graph": graphData
                 }
+        self.saveData(allData)
         return allData
+
+    def getTripGraph(self, location, speed, distance, startEnd):
+        tripTime = self.getDifferenceInTime(startEnd) # In Minutes
+        data = self.loadJson(self.VEGA_GRAPH_SCHEMA_PATH)
+        values = []
+        for minute in range(tripTime):
+            if (minute * 60 < len(speed)):
+                values.append({"time":minute, "speed":speed[minute *60]})
+        data["data"][0]["values"] = values
+        return data
+
+
 
 
 
     def getColorLineParams(self, tripData):
         lineSchemaGeoJson = self.loadJson(self.GEOJSON_LINE_SCHEMA_PATH)
         geoPositionsList = []
-        speedList = []
         distanceList = []
+        speedList = []
+        startEndTime = (tripData["start_time"], tripData["end_time"])
+
         for positionMeta in tripData["coords"]:
             geoPositionsList.append((positionMeta["lat"], positionMeta["lng"]))
             speedList.append(positionMeta["speed"])
             distanceList.append(positionMeta["dist"])
         #lineSchemaGeoJson["features"][0]["geometry"]["coordinates"] = listToAppend
-        return geoPositionsList, speedList, distanceList
+        return geoPositionsList, speedList, distanceList, startEndTime
 
     def loadTripByName(self, fileName):
         fullPath = "{}/{}".format(self.path, fileName)
@@ -47,9 +74,30 @@ class DataProcessing(object):
             exit(1)
         return jsonData
 
+    # Returns time difference in minutes
+    def getDifferenceInTime(self, startEndTime):
+        format = '%Y-%m-%dT%H:%M:%S'
+        start = datetime.strptime(startEndTime[0], format)
+        end = datetime.strptime(startEndTime[1], format)
+
+        timeDiff = end - start
+        return int(round(timeDiff.total_seconds()/60))
 
     def loadJson(self, fullPath):
         with open(fullPath) as f:
             data = json.loads(f.read())
             return data
         return None
+
+    def saveData(self, data):
+        with open(".allData", 'wb') as fp:
+            pickle.dump(data, fp)
+
+    def loadData(self):
+        try:
+            with open (".allData", 'rb') as fp:
+                data = pickle.load(fp)
+                return data
+        except Exception:
+            return False
+        return False
--- MapHandler.py
+++ MapHandler.py
@@ -1,6 +1,6 @@
 import folium
 import math
-
+import json
 from DataProcessing import DataProcessing
 
 class MapHandler(object):
@@ -13,18 +13,24 @@ class MapHandler(object):
 
     def saveExampleMap(self):
         map = folium.Map(location=[37.74977073928103,-122.39242219446099], zoom_start=12, tiles = 'cartodbdark_matter')
-        geo, speed, dist = self.dp.getColorLineParams(self.dp.loadTripByName("2016-07-02--11-56-24.json"))
+        #geo, speed, dist, startEnd = self.dp.getColorLineParams(self.dp.loadTripByName("2016-07-02--11-56-24.json"))
         #folium.GeoJson(geoJsonData, name="2016-07-02--11-56-24").add_to(map)
-        allOrderedData = self.dp.getAllTripsColorLineParams()
-
+        allOrderedData = self.dp.getAllTripsData()
+        count = 0
         for trip in allOrderedData:
+            count += 1
             trip = allOrderedData[trip]
+            fg = folium.FeatureGroup(name=trip["tripName"])
+            fg.add_child(folium.Popup(max_width=750).add_child(folium.Vega(trip["graph"],width=750, height=350)))
             tripColorLine = folium.features.ColorLine(
                 trip["location"],
                 colors = trip["speed"],
                 colormap = ['r', 'orange', 'b'],
-                weight = 3,
-                name = trip["tripName"]
-            )
-            tripColorLine.add_to(map)
+                weight = 1)
+
+            fg.add_child(tripColorLine)
+            map.add_child(fg)
+            break
+
+
         map.save('Map.html')
